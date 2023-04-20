f8bba4d3179b814e8701fa02f598c1f2e2315473
wire-fire <128492219+wire-fire@users.noreply.github.com>
2023-04-20 14:49:27 -0400

    Update Auto_Plant_Watering.ino

--- Auto_Plant_Watering/Auto_Plant_Watering.ino
+++ Auto_Plant_Watering/Auto_Plant_Watering.ino
@@ -1,8 +1,7 @@
-long hrToMs = 3600000; //Converts hours to milliseconds
 long secToMs = 1000; //Converts seconds to milliseconds
 long onSec = 10; //Number of seconds pump should be on
-long offHr = 12; //Number of hours pump should be off
 long relayPin = 2; //Pin connected to water pump relay
+unsigned long hold;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,10 +9,12 @@ void setup() {
   digitalWrite(relayPin, HIGH); //Initialize low
 }
 
-void loop() {
+void loop() { 
   // put your main code here, to run repeatedly:
   digitalWrite(relayPin, LOW); //Turn on pump
   delay(onSec*secToMs); //Delay for onSec seconds
-  digitalWrite(relayPin, HIGH); //Turn off pump
-  delay(offHr*hrToMs); //Delay for offHr hours
+  hold = millis();
+  while (millis()<=(hold+43200000)) {
+    digitalWrite(relayPin, HIGH); //Turn off pump
+  }
 }
