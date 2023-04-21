61fbab2a2cd9c0a75424e268176e16bec2f915f4
wire-fire <jackmctasney03@gmail.com>
2023-04-21 18:13:42 -0400

    Amanda-OLED

--- OLED_Amanda/OLED_Amanda.ino
+++ OLED_Amanda/OLED_Amanda.ino
@@ -7,6 +7,8 @@
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 long percent;
 unsigned long hold;
+long amanda = 0; //God help us
+
 void setup() {
   Serial.begin(9600);
   // initialize OLED display with address 0x3C for 128x64
@@ -19,6 +21,7 @@ void setup() {
 
 void loop() {
   hold = millis();
+  
   while (millis() <= (hold + 10000)) {
     oled.clearDisplay(); // clear display
     oled.setTextSize(2);          // text size
@@ -27,13 +30,34 @@ void loop() {
     percent = ((millis() - hold)/100);
     oled.print(percent);
     oled.print("%");
-    oled.print("(=OwO=)");
+    amanda++;
+    Serial.print(amanda);
+    if (amanda >= 100)
+    {
+      amanda = 0;
+    }
+    
+    if (amanda <= 20)
+    {
+      oled.print("(=UwU=)");
+    }
+    else if ((amanda > 20) && (amanda <= 40))
+    {
+      oled.print("(=Ow^=)");
+    }
+    else if ((amanda > 40) && (amanda <= 60))
+    {
+      oled.print("(=OwO=)");
+    }
+    else if ((amanda > 60) && (amanda <= 80))
+    {
+      oled.print("(=^wO=)");
+    }
+    else if ((amanda > 80) && (amanda <= 100))
+    {
+      oled.print("(=OwO=)");
+    }
     oled.display();
-    Serial.print(percent);
-    Serial.print(" | ");
-    Serial.print(millis());
-    Serial.print(" | ");
-    Serial.println((((millis() - hold) / 10000)*100));
     delay(100);
   }
 }
